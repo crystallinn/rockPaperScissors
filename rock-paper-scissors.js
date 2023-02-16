1e21dd3cd8a1d825e175b6a8f9e783c075361a0d
CrystalLinn <crystal.orc@gmail.com>
2023-02-16 10:07:25 -0800

    fixed variable names

--- rock-paper-scissors.js
+++ rock-paper-scissors.js
@@ -2,17 +2,17 @@ const winner = (userChoice) => {
     let computerChoice = Math.floor(Math.random() * 3);
     if (userChoice === computerChoice){
         result = "It's a tie!";
-    }else if (userChoice === 0 && computer === 1){
+    }else if (userChoice === 0 && computerChoice === 1){
         result = "You Lose";
-    }else if (userChoice === 0 && computer === 2){
+    }else if (userChoice === 0 && computerChoice === 2){
         result = "You Win";
-    }else if (userChoice === 1 && computer === 0){
+    }else if (userChoice === 1 && computerChoice === 0){
         result = "You Win";
-    }else if (userChoice === 1 && computer === 2){
+    }else if (userChoice === 1 && computerChoice === 2){
         result = "You Lose";
-    }else if (userChoice === 2 && computer === 0){
+    }else if (userChoice === 2 && computerChoice === 0){
         result = "You Lose";
-    }else if (userChoice === 2 && computer === 1){
+    }else if (userChoice === 2 && computerChoice === 1){
         result ="You Win";
     }
     document.getElementById('result').innerText = result;
