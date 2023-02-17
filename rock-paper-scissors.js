f516aad132843b8c28020ffc05ca69cbeba25d2c
CrystalLinn <crystal.orc@gmail.com>
2023-02-16 18:29:51 -0800

    The JavaScript is finally working correctly.

--- rock-paper-scissors.js
+++ rock-paper-scissors.js
@@ -1,27 +1,44 @@
 const winner = (userChoice) => {
     let computerChoice = Math.floor(Math.random() * 3);
-    if (userChoice === computerChoice){
-        result = "It's a tie!";
+        // if it is a tie
+    if (userChoice === 0 && computerChoice === 0){
+        result = "You and the computer both chose rock. It's a tie!";
+    }else if (userChoice === 1 && computerChoice === 1){
+            result = "You and the computer both chose paper. It's a tie!";
+    }else if (userChoice === 2 && computerChoice === 2){
+            result = "You and the computer both chose scissors. It's a tie!";
+        // if it is not a tie and the user clicks rock
     }else if (userChoice === 0 && computerChoice === 1){
-        result = "You Lose";
+        result = "You chose rock and the compputer chose paper. You lost.";
     }else if (userChoice === 0 && computerChoice === 2){
-        result = "You Win";
+        result = "You chose rock and the computer chose scissors. You won!";
+        // if it is not a tie and the user clicks paper
     }else if (userChoice === 1 && computerChoice === 0){
-        result = "You Win";
+        result = "You chose paper and the computer chose rock. You won!";
     }else if (userChoice === 1 && computerChoice === 2){
-        result = "You Lose";
+        result = "You chose paper and the computer chose scissors. You lost.";
+        // if it is not a tie and the user clicks scissors
     }else if (userChoice === 2 && computerChoice === 0){
-        result = "You Lose";
+        result = "You chose scissors and the computer chose rock You lost.";
     }else if (userChoice === 2 && computerChoice === 1){
-        result ="You Win";
+        result = "You chose scissors and the computer chose paper. You won!";
+    }
+    else {
+        result = "Something went wrong."
     }
     document.getElementById('result').innerText = result;
     return result;
 }
 
 const rockButton = document.getElementById('rock');
-rockButton.addEventListener('click', winner(0));
+rockButton.addEventListener('click', function () {
+    winner(0);
+});
 const paperButton = document.getElementById('paper');
-paperButton.addEventListener('click', winner(1));
-const scissorsButton = document.getElementById('paper');
-scissorsButton.addEventListener('click', winner(2));
+paperButton.addEventListener('click', function() {
+    winner(1);
+});
+const scissorsButton = document.getElementById('scissors');
+scissorsButton.addEventListener('click', function () {
+    winner(2);
+});
